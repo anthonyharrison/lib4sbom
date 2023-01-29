a260205bfdf0d19ed376e21ecaee6842a6b2bb99
anthonyharrison <anthony.p.harrison@gmail.com>
2023-01-29 22:13:28 +0000

    fix: Update testing code

--- examples/summary.py
+++ examples/summary.py
@@ -11,27 +11,32 @@ from lib4sbom.data.document import SBOMDocument
 
 test_parser = SBOMParser()
 # Load SBOM
-test_parser.parse_file(sys.argv[1])
+try:
+    test_parser.parse_file(sys.argv[1])
 
-# What type of SBOM
-document = SBOMDocument()
-print (test_parser.get_document())
-document.copy_document(test_parser.get_document())
+    # What type of SBOM
+    document = SBOMDocument()
+    print (test_parser.get_document())
+    document.copy_document(test_parser.get_document())
 
-packages = test_parser.get_packages()
-files = test_parser.get_files()
-print ("Summary")
-print ("=" * len("summary"))
-print (f"SBOM Type    {document.get_type()}")
-print (f"Version      {document.get_version()}")
-print (f"Name         {document.get_name()}")
-print ()
-print (f"Files        {len(files)}")
-for file in files:
-    print (f"{file['name']:30} - {file.get('type','NOT DEFINED'):20}")
-print (f"\nPackages     {len(packages)}")
-print (f"\n{'Name':30} {'Version':15} {'Type':20}")
-print ("-" * 70)
-for package in packages:
-    print (f"{package['name']:30} {package.get('version','MISSING'):15} {package['type']:20}")
+    packages = test_parser.get_packages()
+    files = test_parser.get_files()
+    print ("Summary")
+    print ("=" * len("summary"))
+    print (f"SBOM Type    {document.get_type()}")
+    print (f"Version      {document.get_version()}")
+    print (f"Name         {document.get_name()}")
+    print ()
+    print (f"Files        {len(files)}")
+    for file in files:
+        print (f"{file['name']:30} - {file.get('type','NOT DEFINED'):20}")
+    print (f"\nPackages     {len(packages)}")
+    print (f"\n{'Name':30} {'Version':15} {'Type':20}")
+    print ("-" * 70)
+    for package in packages:
+        print (f"{package['name']:30} {package.get('version','MISSING'):15} {package['type']:20}")
+
+except FileNotFoundError:
+    print (f"{sys.argv[1]} not found")
+
 
