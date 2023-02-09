2729af9e4488092a88954c5cbfb5eb1e6cfdf3d5
anthonyharrison <anthony.p.harrison@gmail.com>
2023-02-09 23:00:55 +0000

    fix: Update test

--- test/test_relationship.py
+++ test/test_relationship.py
@@ -12,7 +12,7 @@ class TestRelationship:
 	def test_set_relationship(self):
 		test_item = test_module()
 		test_item.set_relationship("source_module","a_relationship","target_module")
-		assert test_item.get_relationship() == {'source' : 'source_module', 'type': 'a_relationship', 'target': 'target_module'}
+		assert test_item.get_relationship() == {'source' : 'source_module', 'type': 'a_relationship', 'target': 'target_module', 'source_id': None, 'target_id': None}
 
 	def test_get_source(self):
 		test_item = test_module()
