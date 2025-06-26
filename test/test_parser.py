7d231a2e691c3a83b0b5a898ab34a8213fb52894
Sergei <ivanov.cmn@gmail.com>
2025-06-27 01:06:22 +0300

    Fixed tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -7,18 +7,26 @@ from lib4sbom.parser import SBOMParser
 class TestParser:
     def test_parse_file(self):
         files = []
-        for (dirpath, _, filenames) in os.walk('samples'):
+        for (dirpath, _, filenames) in os.walk('../samples'):
             files.extend([os.path.join(dirpath, x) for x in filenames])
+        for (dirpath, _, filenames) in os.walk('./data'):
+            files.extend([os.path.join(dirpath, x) for x in filenames])
+            break
         for f in files:
+            print(f)
             a = SBOMParser()
             a.parse_file(f)
             assert a.get_packages()
 
     def test_parse_string(self):
         files = []
-        for (dirpath, _, filenames) in os.walk('samples'):
+        for (dirpath, _, filenames) in os.walk('../samples'):
             files.extend([os.path.join(dirpath, x) for x in filenames])
+        for (dirpath, _, filenames) in os.walk('./data'):
+            files.extend([os.path.join(dirpath, x) for x in filenames])
+            break
         for f in files:
+            print(f)
             a = SBOMParser()
             with open(f, "r", encoding="utf-8") as f:
                 sbom_string = f.read()
