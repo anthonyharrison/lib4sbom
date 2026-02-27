0a8c5cde2de96fe70c51f88a009d356d8072ddf0
anthonyharrison <anthony.p.harrison@gmail.com>
2026-02-27 11:52:53 +0000

    fix: distribution constraints only for version 1.7

--- lib4sbom/cyclonedx/cyclonedx_generator.py
+++ lib4sbom/cyclonedx/cyclonedx_generator.py
@@ -224,10 +224,11 @@ class CycloneDXGenerator:
                 metadata_property.append(property_entry)
             metadata["properties"] = metadata_property
         metadata["component"] = component
-        if component_type["distribution"] is not None:
-            distributionConstraints = dict()
-            distributionConstraints["tlp"] = component_type["distribution"]
-            metadata["distributionConstraints"] = distributionConstraints
+        if self._cyclonedx_17():
+            if component_type["distribution"] is not None:
+                distributionConstraints = dict()
+                distributionConstraints["tlp"] = component_type["distribution"]
+                metadata["distributionConstraints"] = distributionConstraints
         self.doc["metadata"] = metadata
         return component["bom-ref"]
 
